e1934ddb011ed68f832921adeec73dd20ebdb9a9
barbvilla <barbara.villasenor.c@gmail.com>
2023-09-29 11:10:13 -0300

    proyecto listo

--- src/API.js
+++ src/API.js
@@ -1,13 +1,35 @@
 const carouselInner = document.getElementById('cards');
+const typeNavContainer = document.getElementById('type-nav');
 
 fetch('https://pokeapi.co/api/v2/type/')
   .then(res => res.json())
   .then(typesData => {
     const types = typesData.results.map(type => type.name);
     types.forEach(type => {
-      getPokemons(type);
+      createTypeButton(type);
     });
   });
+
+function createTypeButton(type) {
+  const typeButton = document.createElement('button');
+  typeButton.textContent = type.toUpperCase();
+  typeButton.classList.add('type-button');
+  typeButton.dataset.type = type;
+
+  typeButton.addEventListener('click', (event) => {
+    const selectedType = event.target.dataset.type;
+    clearCarousel();
+    getPokemons(selectedType);
+  });
+
+  typeNavContainer.appendChild(typeButton);
+}
+
+const clearCarousel = () => {
+  while (carouselInner.firstChild) {
+    carouselInner.removeChild(carouselInner.firstChild);
+  }
+};
 
 const getPokemons = (type) => {
   const promises = [];
@@ -30,7 +52,7 @@ const getPokemons = (type) => {
     });
 }
 
-function hasType(types, t) {
+const hasType = (types, t) => {
   return types.some((el) => el.type.name === t);
 }
 
@@ -38,31 +60,57 @@ const showPokemon = (pokemon, type) => {
   const pokemonCard = pokemon.map(forPokemon =>
     `<div class="col-auto">
       <div class="card card${forPokemon.id}" style="width: 18rem;">
-        <img src="${forPokemon.image}" class="card-img-top" alt="${pokemon.name}" />
+        <img src="${forPokemon.image}" class="card-img-top" alt="${forPokemon.name}" />
         <div class="card-body">
           <h4 class="card-title">${forPokemon.name}</h2>
         </div>
       </div>
-    </div>`).join("")
+    </div>`).join("");
 
-    
-    if (pokemon.length > 0) {
-      const typeSection = document.createElement('div');
-      typeSection.classList.add('type-section');
-      typeSection.innerHTML = `<h3 class="type-title-${type}">${type.toUpperCase()} POKEMON</h2>`;
-      typeSection.innerHTML += `<div class="row">${pokemonCard}</div>`;
-  
-      carouselInner.appendChild(typeSection);
-      
-      $(typeSection.querySelector('.row')).slick({
-        infinite: true, 
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        dots: true,
-        arrow: true,
-        mobileFirst: true,
-      });
-    }    
+  if (pokemon.length > 0) {
+    const typeSection = document.createElement('div');
+    typeSection.classList.add('type-section');
+    typeSection.innerHTML = `<h3 class="type-title-${type}">${type.toUpperCase()} POKEMON</h2>`;
+    typeSection.innerHTML += `<div class="row">${pokemonCard}</div>`;
+
+    carouselInner.appendChild(typeSection);
+
+    $(typeSection.querySelector('.row')).slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      dots: true,
+      arrows: true,
+      responsive: [
+        {
+          breakpoint: 1290,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          },
+        },
+        {
+          breakpoint: 900,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 790,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+  }
 };
-
-getPokemons();
