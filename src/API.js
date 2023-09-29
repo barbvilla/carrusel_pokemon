bd459d6291110aa4256c4bcc561743583256d3f2
barbvilla <barbara.villasenor.c@gmail.com>
2023-09-29 04:09:04 -0300

    filtro funcionando con un tipo

--- src/API.js
+++ src/API.js
@@ -21,27 +21,27 @@ const getPokemons = () => {
       return pokemon
     })
     .then(res => {
+      const poisonedPokemon = [];
       res.forEach((pokemon) => {
-        const filteredPokemon = hasType(pokemon.types, "poison")
-        console.log(filteredPokemon);
-      })
-      
+        if (hasType(pokemon.types, "grass")) {
+          poisonedPokemon.push(pokemon);
+        }
+      });
+      showPokemon(poisonedPokemon);
     })
 }
 
-
 function hasType(types, t) {
-  /* console.log(types)
-  console.log(t) */
-  const filterByType = types.filter((el) => el.type.name.includes(t))
-  return filterByType;
+  const filterByType = types.some((el) => el.type.name === t);
+  return filterByType
+  
 }
 
 /* const filterType = Object.groupBy(pokemon, ({ type }) => type);
   console.log(filterType); */
 
 const showPokemon = (pokemon) => {
-  const pokemonCard = pokemon.map(forPokemon => 
+  const pokemonCard = pokemon.map(forPokemon =>
     `<div class="col">
       <div class="card card${forPokemon.id}" style="width: 18rem;">
         <img src="${forPokemon.image}" class="card-img-top" alt="${pokemon.name}" />
